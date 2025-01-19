7cd1c30619fc04a7a2af1af84139b0e1fa29572f
dayvisonmsilva <dayvisoneryc@gmail.com>
2025-01-18 22:02:34 -0300

    Configura o Vite para escutar em todas as interfaces de rede

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Escuta em todas as interfaces
+    port: 3000, // (Opcional) Define uma porta específica
+  },
 })
